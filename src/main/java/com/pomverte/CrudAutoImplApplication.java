7d0abd9190fe9c1b46951ec5d5ae5aa0b42af689
pomverte <vietnem@yahoo.fr>
2015-12-07 23:41:30 +0100

    init

---
+++ src/main/java/com/pomverte/CrudAutoImplApplication.java
@@ -0,0 +1,12 @@
+package com.pomverte;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudAutoImplApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrudAutoImplApplication.class, args);
+    }
+}
